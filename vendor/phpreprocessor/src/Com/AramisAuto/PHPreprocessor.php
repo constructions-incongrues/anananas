3ef72f8bcf6e2756800d88b30d64cf779cb09265
Tristan Rivoallan <tristan@rivoallan.net>
2013-10-01 12:00:57 +0200

    [phpreprocessor] new property "phpreprocessor.apply.timestamp"

--- vendor/phpreprocessor/src/Com/AramisAuto/PHPreprocessor.php
+++ vendor/phpreprocessor/src/Com/AramisAuto/PHPreprocessor.php
@@ -76,7 +76,7 @@ class PHPreprocessor
 
         // Unserialize tokens
         $tokensFiles = explode(',', $options['properties']);
-        $tokens = array();
+        $tokens = array('phpreprocessor.apply.timestamp' => time());
         foreach ($tokensFiles as $tokenFile)
         {
             if (!is_readable($tokenFile))
